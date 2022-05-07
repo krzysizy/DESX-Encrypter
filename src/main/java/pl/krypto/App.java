a3903fb8a719c639c59fa7c17caa18ee6fce74eb
Krzysztof Izydorczyk <236545@edu.p.lodz.pl>
2022-05-08 01:11:23 +0200

    Controller

--- src/main/java/pl/krypto/App.java
+++ src/main/java/pl/krypto/App.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import pl.krypto.cast.Permutation;
 
 import java.util.Objects;
 
@@ -20,5 +21,14 @@ public class App extends Application {
         Scene scene = new Scene(root, 659, 579);
         primaryStage.setScene(scene);
         primaryStage.show();
+//        byte [] data = {0,2};
+//        byte [] patern = {14,13,10,2,3,4,5,8};
+//
+//        System.out.println(Permutation.getBit(data,14));
+//        Permutation.setBit(data,13,1);
+//        Permutation.setBit(data,14,0);
+//        System.out.println(Permutation.getBit(data,13));
+//        System.out.println(Permutation.getBit(data,14));
+//        System.out.println(Permutation.getBit(Permutation.permutation(data,patern),1));
     }
 }
--- src/main/java/pl/krypto/graficControllers/Controller.java
+++ src/main/java/pl/krypto/graficControllers/Controller.java
@@ -1,12 +1,46 @@
 package pl.krypto.graficControllers;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class Controller {
-    @FXML
-    private Label buttonLabel1;
 
-    public void sayHello() {
-        buttonLabel1.setText("Hello FXML!");
-    }
+    @FXML
+    private TextField Key1Text;
+
+    @FXML
+    private TextField Key2Text;
+
+    @FXML
+    private TextField Key3Text;
+
+    @FXML
+    private TextArea TextToEncrypt;
+
+    @FXML
+    private TextArea EncryptedText;
+
+    @FXML
+    private Button EncryptTextButton;
+
+    @FXML
+    private Button DecryptTextButton;
+
+    @FXML
+    private Button EncryptFileButton;
+
+    @FXML
+    private Button DecryptFileButton;
+
+    @FXML
+    private TextField FileNameToEncrypt;
+
+    @FXML
+    private Label FileEncryptionSuccessLabel;
+
+    @FXML
+    private ProgressBar ProgressBar;
+    private double progress = 0;
+
+    
+
 }
